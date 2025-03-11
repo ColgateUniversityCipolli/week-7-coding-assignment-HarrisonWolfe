9b620198568192e969e7e3566e07d0855785f2e7
HarrisonWolfe <hwolfe@colgate.edu>
2025-03-11 17:42:53 -0400

    trying to finish step 5

--- ScriptforHW7.R
+++ ScriptforHW7.R
@@ -1,7 +1,11 @@
 
 pois.prob <- function(x, lambda, type="<="){
-  if(x<0){
+ x = floor(x)
+   if(x<0){
     return("x must be a positive integer")
+  }
+  if(lambda < 0){
+    return("lambda must be a positive integer")
   }
   if(type == "<="){#Less than or equal to
     return(ppois(x,lambda))
@@ -22,5 +26,34 @@ pois.prob <- function(x, lambda, type="<="){
 }
 
 
+beta.prob <- function(x, alpha, beta, type="<="){
+  if(x<0){
+    return("x must be positive")
+  }
+  if(alpha<0){
+    return("alpha must be positive")
+  }
+  if(beta<0){
+    return("beta must be postive")
+  }
+  if(type == "<="){#Less than or equal to
+    return(pbeta(x,alpha,beta))
+  }
+  if(type == "="){#Equal to
+    return(0)
+  }
+  if(type == "!="){#Not equal to
+    return(1)
+  }
+  if(type == ">="){# Greater than or equal to
+    return(1-pbeta(x,alpha,beta))
+    }
+  if(type == ">"){# Greater than
+    return(1-pbeta(x,alpha,beta))
+  }
+  if(type == "<"){#Less than
+    return(pbeta(x,alpha,beta))
+  }
+}
 
 
